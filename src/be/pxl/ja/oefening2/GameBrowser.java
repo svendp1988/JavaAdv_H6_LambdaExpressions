618e6e913b56894c4d983562f3403e8459fccf63
Nele Custers <custersnele@gmail.com>
2020-10-27 19:13:19 +0100

    startbestanden voor oefeningen

---
+++ src/be/pxl/ja/oefening2/GameBrowser.java
@@ -0,0 +1,23 @@
+package be.pxl.ja.oefening2;
+
+import java.util.List;
+
+public class GameBrowser {
+
+	// TODO: implement this class
+
+	public GameBrowser(GameCollection gameCollection) {
+	}
+
+	public List<VideoGame> showFreeGames() {
+		return null;
+	}
+
+	public List<VideoGame> showGamesInGenre(String action) {
+		return null;
+	}
+
+	public List<VideoGame> showGamesForSearch(String cd) {
+		return null;
+	}
+}
---
+++ src/be/pxl/ja/oefening2/GameCollection.java
@@ -0,0 +1,9 @@
+package be.pxl.ja.oefening2;
+
+public class GameCollection {
+
+	public void addGame(VideoGame game) {
+
+	}
+
+}
